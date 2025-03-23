05ec92d6d7915fe3b4209110ff6d35589574900a
Yulin <y.chen-19@student.utwente.nl>
2025-03-23 14:33:15 +0100

    Fixed range setting issue

--- IRutils/models.py
+++ IRutils/models.py
@@ -1,16 +1,16 @@
 from torch import nn
-from transformers import DistilBertModel
+from transformers import AutoModel
 
 
 class TripletRankerModel(nn.Module):
     def __init__(self, model_name):
         super(TripletRankerModel, self).__init__()
-        self.distilbert = DistilBertModel.from_pretrained(model_name)
+        self.model = AutoModel.from_pretrained(model_name)
         self.linear = nn.Linear(768, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, input_ids, attention_mask):
-        outputs = self.distilbert(input_ids=input_ids, attention_mask=attention_mask)
+        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask)
         logits = self.linear(outputs.last_hidden_state[:, 0, :])  # CLS token
         return self.sigmoid(logits)
 
